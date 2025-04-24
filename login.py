68c487813b67f6fa3933e915a6d7b21fa2e9b7d0
beeshaker <abhishekshah546@gmail.com>
2025-04-24 16:00:20 +0300

    menu update

--- login.py
+++ login.py
@@ -2,9 +2,9 @@ import streamlit as st
 from sqlalchemy.sql import text
 import bcrypt
 from conn import Conn
-
+# Page Configuration
+st.set_page_config(page_title="CRM Admin Portal", layout="wide")
 db = Conn()
-
 
 # Custom CSS for Dark Mode Styling
 st.markdown("""
